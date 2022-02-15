cb26085b3334a4c9234907c2ba07eba7ecdb6d73
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-02-15 21:28:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,8 @@
 
 # git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
-git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
+git clone -b dev https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
+# git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
