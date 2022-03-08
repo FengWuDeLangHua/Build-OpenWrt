a7e38e4e627028169d8824a27a581f0743cbee0b
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-03-08 10:11:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@ git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/lean/openwrt-mos
 git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
