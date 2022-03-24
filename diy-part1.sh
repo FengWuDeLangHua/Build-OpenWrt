e4c2a3e0869a6d658a1c49f3f352a161e554e972
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-03-24 14:23:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/openwrt-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/lean/openwrt-passwall
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/lean/openwrt-mos
 git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
