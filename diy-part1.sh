b2275f69d62f547b7a44c53794f5896d43ac21bc
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-04-02 01:12:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,6 +2,9 @@
 
 # rm -rf feeds/packages/net/smartdns
 
+# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+
 # svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns feeds/packages/net/smartdns
 # svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-smartdns package/lean/luci-app-smartdns
 # svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-aliddns package/lean/luci-app-aliddns
@@ -9,5 +12,4 @@
 # sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/lean/luci-app-smartdns/Makefile
 # sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/lean/luci-app-aliddns/Makefile
 
-# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+
