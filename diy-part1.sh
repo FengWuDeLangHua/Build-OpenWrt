964197fb10d46b43c18bc71899a7cfb03b09cbf8
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-04-02 01:00:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,12 +4,9 @@ rm -rf feeds/packages/net/smartdns
 
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns feeds/packages/net/smartdns
 svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-aliddns feeds/luci/applications/luci-app-aliddns
 
 git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall2 package/lean/luci-app-passwall2
-# git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/lean/openwrt-passwall
-# git clone https://github.com/QiuSimons/openwrt-mos.git package/lean/openwrt-mos
-git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
+# git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
