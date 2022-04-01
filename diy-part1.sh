ba9a669ce6aefc2b7e21e4d807a43ae9ea5e068f
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-04-01 17:40:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 # git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall2 package/lean/luci-app-passwall2
 # git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/lean/openwrt-passwall
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/lean/openwrt-mos
-git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+# git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
