2f571bc358bc365e1dc25efff4d902aac0a9e607
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-04-06 14:40:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -rf feeds/packages/net/smartdns
+# rm -rf feeds/packages/net/smartdns
 
-git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
 
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns feeds/packages/net/smartdns
-svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-smartdns package/lean/luci-app-smartdns
-svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-aliddns package/lean/luci-app-aliddns
+# svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns feeds/packages/net/smartdns
+# svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-smartdns package/lean/luci-app-smartdns
+# svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-aliddns package/lean/luci-app-aliddns
 
-sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/lean/luci-app-smartdns/Makefile
-sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/lean/luci-app-aliddns/Makefile
+# sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/lean/luci-app-smartdns/Makefile
+# sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/lean/luci-app-aliddns/Makefile
