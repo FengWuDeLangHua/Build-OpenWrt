3dd5bd4b2abe351cae014c3cfc9268d7d0d331dc
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-04-14 22:41:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,8 +2,8 @@
 
 # rm -rf feeds/packages/net/smartdns
 
-# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
 
 # svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns feeds/packages/net/smartdns
 # svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-smartdns package/lean/luci-app-smartdns
