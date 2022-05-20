44a9d88d53bcf313f76abbcbde2dc19d3e1ba016
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2022-05-20 09:45:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,9 +2,9 @@
 
 # rm -rf feeds/packages/net/smartdns
 
-# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
-# git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
+git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
