12442b18ac124a280acca5b5821ce5e6d123325d
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2023-01-19 19:09:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/PATCHVER:=5.15/PATCHVER:=5.19/g' target/linux/x86/Makefile
+sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/x86/Makefile
 # sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' target/linux/x86/Makefile
 
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
