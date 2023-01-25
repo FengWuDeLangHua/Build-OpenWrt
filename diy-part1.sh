1eece075ab15491761f37c0214d9afed3b8e9206
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2023-01-25 22:55:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,5 +10,6 @@ git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 # git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
 # git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
-git clone https://github.com/pymumu/openwrt-smartdns.git package/lean/openwrt-smartdns
+# git clone https://github.com/pymumu/openwrt-smartdns.git package/lean/openwrt-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
