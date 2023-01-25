14912775c233200f0beeb5aea51ece1ba6f839a6
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2023-01-25 18:07:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,11 +3,11 @@
 # rm -rf feeds/packages/net/smartdns
 
 # git clone https://github.com/kenzok8/small-package package/lean/small-package
-# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+# git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
 # git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
