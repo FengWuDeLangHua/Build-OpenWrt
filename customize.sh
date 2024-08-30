a4d1791ed890f63e8ee5482a01dce553a9441f24
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2024-08-30 22:53:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,24 +6,24 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
-rm package/lean/luci-theme-argon -rf
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-jerrykuku
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+rm openwrt/package/lean/luci-theme-argon -rf
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon-jerrykuku
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
 
 #4
-git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+git clone https://github.com/fw876/helloworld.git openwrt/package/lean/helloworld
 
-rm -rf feeds/packages/net/smartdns -rf
-rm -rf feeds/luci/applications/luci-app-smartdns -rf
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+rm -rf openwrt/feeds/packages/net/smartdns -rf
+rm -rf openwrt/feeds/luci/applications/luci-app-smartdns -rf
+git clone https://github.com/pymumu/openwrt-smartdns.git openwrt/feeds/packages/net/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git openwrt/package/lean/luci-app-smartdns
 
-rm -rf feeds/packages/net/mosdns -rf
-rm -rf feeds/luci/applications/luci-app-mosdns -rf
-git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
+rm -rf openwrt/feeds/packages/net/mosdns -rf
+rm -rf openwrt/feeds/luci/applications/luci-app-mosdns -rf
+git clone https://github.com/sbwml/luci-app-mosdns.git openwrt/package/lean/luci-app-mosdns
