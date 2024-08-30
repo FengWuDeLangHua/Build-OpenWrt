3cc6c6bea5d283061134a718c2cdb253060d8fe2
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2024-08-30 21:45:58 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
