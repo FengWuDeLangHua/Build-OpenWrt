334b5d907152768b1aa2c784d6a0fe79b7be9a59
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2024-08-30 22:40:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -17,11 +17,12 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 #4
+git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+
 rm -rf feeds/packages/net/smartdns -rf
 rm -rf feeds/luci/applications/luci-app-smartdns -rf
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
-
 
 rm -rf feeds/packages/net/mosdns -rf
 rm -rf feeds/luci/applications/luci-app-mosdns -rf
