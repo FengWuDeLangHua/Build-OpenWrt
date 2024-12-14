efcc62ae8e2bbf02d71fa2dbadd6bb9d4cf86003
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2024-12-14 19:44:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,33 +9,14 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' openwrt/package/emortal/default-settings/files/99-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' openwrt/package/emortal/default-settings/files/99-default-settings
 
 # #3. Replace with JerryKuKu’s Argon
 rm -rf openwrt/feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/feeds/luci/themes/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci-light/Makefile
 
 # #4
-# git clone https://github.com/fw876/helloworld.git openwrt/package/lean/helloworld
-
-# rm -rf openwrt/feeds/packages/net/smartdns
-# rm -rf openwrt/feeds/luci/applications/luci-app-smartdns
-# git clone https://github.com/pymumu/openwrt-smartdns.git openwrt/feeds/packages/net/smartdns
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git openwrt/package/emortal/luci-app-smartdns
-
-# rm -rf openwrt/feeds/packages/net/mosdns
-# rm -rf openwrt/feeds/luci/applications/luci-app-mosdns
-# git clone https://github.com/sbwml/luci-app-mosdns.git openwrt/package/emortal/luci-app-mosdns
-
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 openwrt/package/emortal/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata openwrt/package/emortal/v2ray-geodata
-
 rm -rf openwrt/feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/emortal/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/lean/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt/package/lean/openwrt-passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/emortal/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt/package/emortal/openwrt-passwall-packages
