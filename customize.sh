9b654f3089dd6a50696411876a1acea0be0c87dd
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-02-08 23:50:30 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/luci2/bin/config_generate
 
 #2. Clear the login password
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' openwrt/package/emortal/default-settings/files/99-default-settings
