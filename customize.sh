170202b3fa9a51b6f3e5ce7cda921a744ab45bc7
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-02-09 02:29:27 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,3 +16,15 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' openwrt/package/lean/default-settings/files/zz
 rm -rf openwrt/feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/feeds/luci/themes/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci-light/Makefile
+
+# #4
+rm -rf openwrt/feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git openwrt/package/lean/luci-app-smartdns
+
+rm -rf openwrt/feeds/luci/applications/luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/emortal/openwrt-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt/package/emortal/openwrt-passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/package/lean/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt/package/lean/openwrt-passwall-packages
+
+
