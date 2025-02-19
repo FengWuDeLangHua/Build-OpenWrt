d5d635b98caeaa41e31ef07eb7dc6c4067b002c9
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-02-19 21:15:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,25 +6,25 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 
 #2. Clear the login password
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' openwrt/package/emortal/default-settings/files/99-default-settings
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' openwrt/package/lean/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/"终端"/"TTYD"/g' openwrt/feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 #3. Replace with JerryKuKu’s Argon
-rm -rf openwrt/feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/feeds/luci/themes/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci-light/Makefile
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 #4
-rm -rf openwrt/feeds/luci/applications/luci-app-passwall
-rm -rf openwrt/feeds/luci/applications/luci-app-passwall2
-rm -rf openwrt/feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,microsocks,pdnsd-alt}
-rm -rf openwrt/feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang openwrt/feeds/packages/lang/golang
-git clone https://github.com/xiaorouji/openwrt-passwall.git openwrt/feeds/luci/applications/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt/feeds/packages/net/openwrt-passwall-package
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,microsocks,pdnsd-alt}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/xiaorouji/openwrt-passwall.git feeds/luci/applications/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git feeds/packages/net/openwrt-passwall-package
 
