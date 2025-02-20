2c045c0e6ae0a2b2350ff18d9307d399df734a0b
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-02-21 00:05:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -23,10 +23,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-l
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,microsocks,pdnsd-alt}
-rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-$svn https://github.com/kenzok8/small/trunk/v2dat feeds/packages/utils/v2dat
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/lean/openwrt-passwall-package
 cp -Rf package/lean/openwrt-passwall-package/* feeds/packages/net/
