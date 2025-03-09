2dd7e992db00a3a481e385421f22e0fd0a2df895
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-03-09 16:47:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,8 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
