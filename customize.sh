e5575d824acd219a83063268c7d6926dd54ad270
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-03-09 16:45:39 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,10 +9,10 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 
 #2. Clear the login password
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+# sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 #3. Replace with JerryKuKu’s Argon
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -20,9 +20,9 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/lu
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 #4
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall2
+# rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/luci/applications/luci-app-passwall2
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/lean/openwrt-passwall-package
-git clone https://github.com/fw876/helloworld.git package/lean/helloworld
+# git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 
