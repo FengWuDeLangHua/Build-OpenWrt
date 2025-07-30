3279fcc842fc9d45b6029c8afcffca2c6dee18bb
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-07-30 15:01:29 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -22,9 +22,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-l
 
 #4
 rm -rf feeds/luci/applications/luci-app-passwall
-# rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/luci/applications/luci-app-openclash
-# rm -rf feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
 # git clone https://github.com/immortalwrt/homeproxy.git feeds/luci/applications/luci-app-homeproxy
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/lean/openwrt-passwall-package
