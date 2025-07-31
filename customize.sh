237bdccbcdbbcce6237132416f894ae6fcf00d64
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-07-31 09:13:38 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,8 +16,8 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-defa
 # sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 #3. Replace with JerryKuKu’s Argon
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 #4
