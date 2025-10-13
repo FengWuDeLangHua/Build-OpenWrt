49a156b1cfa91046ef166daea3cf172449aaf6f0
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-10-13 23:45:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -32,12 +32,12 @@ rm -rf feeds/luci/applications/luci-app-homeproxy
 
 # daed
 rm -rf feeds/luci/applications/luci-app-dae
-# rm -rf feeds/luci/applications/luci-app-daed
-# git clone https://github.com/QiuSimons/luci-app-daed package/dae
-# mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
-# apt-get update
-# apt-get install -y clang-15 llvm-15 npm
-# npm install -g pnpm
+rm -rf feeds/luci/applications/luci-app-daed
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
+mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
+apt-get update
+apt-get install -y clang-15 llvm-15 npm
+npm install -g pnpm
 echo -e "\nCONFIG_DEVEL=y" >> .config
 echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
 echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
