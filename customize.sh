a43027d5ab784bfb90d312a115f3c2f653bd7e60
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-10-13 19:27:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -38,16 +38,6 @@ mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubu
 apt-get update
 apt-get install -y clang-15 llvm-15 npm
 npm install -g pnpm
-echo -e "\nCONFIG_DEVEL=y" >> .config
-echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
-echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
-echo -e "\nCONFIG_KERNEL_DEBUG_INFO_BTF=y" >> .config
-echo -e "\nCONFIG_KERNEL_CGROUPS=y" >> .config
-echo -e "\nCONFIG_KERNEL_CGROUP_BPF=y" >> .config
-echo -e "\nCONFIG_KERNEL_BPF_EVENTS=y" >> .config
-echo -e "\nCONFIG_BPF_TOOLCHAIN_HOST=y" >> .config
-echo -e "\nCONFIG_KERNEL_XDP_SOCKETS=y" >> .config
-echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
 
 # passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
