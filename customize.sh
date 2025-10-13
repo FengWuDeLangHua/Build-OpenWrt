963476fbbc766711a7a5cc2a9dbb50900c7e4c9d
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-10-13 18:06:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,43 +7,47 @@
 #=================================================
 #1. Modify default IP
 # lede
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 # immortalwrt
-# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 # lede
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 # immortalwrt
-# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
 
 # sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 #3. Replace with JerryKuKu’s Argon
 # rm -rf feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
 
 #4
-rm -rf feeds/luci/applications/luci-app-passwall2
+# openclash
 rm -rf feeds/luci/applications/luci-app-openclash
+
+# homeproxy
 rm -rf feeds/luci/applications/luci-app-homeproxy
+
+# dae
 rm -rf feeds/luci/applications/luci-app-dae
-# git clone https://github.com/immortalwrt/homeproxy.git feeds/luci/applications/luci-app-homeproxy
-# git clone https://github.com/fw876/helloworld.git package/emortal/helloworld
-# git clone https://github.com/sbwml/luci-app-mosdns.git package/emortal/luci-app-mosdns
-# 移除 openwrt feeds 过时的luci版本
+
+# passwall
 rm -rf feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/luci/applications/luci-app-passwall2
 # rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
 # smartdns
 # rm -rf feeds/packages/net/smartdns
 # rm -rf feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+
 # nikki
 # git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
