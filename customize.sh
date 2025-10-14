f7e2b9e414f7a0dc51be4d03a407cce070fb562c
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-10-14 11:07:48 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,6 +12,8 @@
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # Lienol
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# openwrt
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 # lede
@@ -20,7 +22,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # immortalwrt
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
 # Lienol
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
