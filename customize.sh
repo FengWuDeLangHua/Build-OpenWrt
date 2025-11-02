ab97fb4b3ac68be481d4e99851ec547d83570c40
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-11-02 19:45:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -61,7 +61,7 @@ echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
 
 # passwall
 # rm -rf feeds/luci/applications/luci-app-passwall2
-# rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
 # rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
