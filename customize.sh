aff455fb2a89fa87885a9f802cbe92baaa12acc7
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-12-04 20:17:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -45,9 +45,14 @@ rm -rf feeds/packages/net/dae
 rm -rf feeds/packages/net/daed
 git clone https://github.com/QiuSimons/luci-app-daed package/dae
 mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
-apt-get update
-apt-get install -y clang-15 llvm-15 npm
+# apt-get update
+# apt-get install -y clang-15 llvm-15 npm
+# npm install -g pnpm
+apt-get -qq update
+apt-get -qq install clang llvm npm
 npm install -g pnpm
+npm install -g n
+npm i taze -g
 echo -e "\nCONFIG_DEVEL=y" >> .config
 echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
 echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
