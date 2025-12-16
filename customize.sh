87085eaf4435aa184d67cc2847bd4e0ec8975add
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2025-12-16 11:31:37 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -41,23 +41,23 @@ rm -rf feeds/luci/applications/luci-app-homeproxy
 # daed
 rm -rf feeds/luci/applications/luci-app-dae
 rm -rf feeds/luci/applications/luci-app-daed
-rm -rf feeds/packages/net/dae
-rm -rf feeds/packages/net/daed
-git clone https://github.com/QiuSimons/luci-app-daed package/dae
-# mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
-sudo apt-get update
-sudo apt-get install clang-15 llvm-15 npm
-sudo npm install -g pnpm
-echo -e "\nCONFIG_DEVEL=y" >> .config
-echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
-echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
-echo -e "\nCONFIG_KERNEL_DEBUG_INFO_BTF=y" >> .config
-echo -e "\nCONFIG_KERNEL_CGROUPS=y" >> .config
-echo -e "\nCONFIG_KERNEL_CGROUP_BPF=y" >> .config
-echo -e "\nCONFIG_KERNEL_BPF_EVENTS=y" >> .config
-echo -e "\nCONFIG_BPF_TOOLCHAIN_HOST=y" >> .config
-echo -e "\nCONFIG_KERNEL_XDP_SOCKETS=y" >> .config
-echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
+# rm -rf feeds/packages/net/dae
+# rm -rf feeds/packages/net/daed
+# git clone https://github.com/QiuSimons/luci-app-daed package/dae
+# # mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
+# sudo apt-get update
+# sudo apt-get install clang-15 llvm-15 npm
+# sudo npm install -g pnpm
+# echo -e "\nCONFIG_DEVEL=y" >> .config
+# echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
+# echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
+# echo -e "\nCONFIG_KERNEL_DEBUG_INFO_BTF=y" >> .config
+# echo -e "\nCONFIG_KERNEL_CGROUPS=y" >> .config
+# echo -e "\nCONFIG_KERNEL_CGROUP_BPF=y" >> .config
+# echo -e "\nCONFIG_KERNEL_BPF_EVENTS=y" >> .config
+# echo -e "\nCONFIG_BPF_TOOLCHAIN_HOST=y" >> .config
+# echo -e "\nCONFIG_KERNEL_XDP_SOCKETS=y" >> .config
+# echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
 
 # passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
@@ -73,4 +73,4 @@ rm -rf feeds/luci/applications/luci-app-passwall
 # git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
 
 # nikki
-# git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
