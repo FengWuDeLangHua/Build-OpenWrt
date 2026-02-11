c0a354f8e51f093ad33dc838b07c1c4b99da50b7
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2026-02-11 14:17:04 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -63,17 +63,17 @@ rm -rf feeds/luci/applications/luci-app-daed
 # echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
 
 # passwall
-rm -rf feeds/luci/applications/luci-app-passwall2
-# rm -rf feeds/luci/applications/luci-app-passwall
-# rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+# rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
 # smartdns
-# rm -rf feeds/packages/net/smartdns
-# rm -rf feeds/luci/applications/luci-app-smartdns
-# git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-# git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
 
 # nikki
 # git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
