f5ca3007db3bde05fbdd005fa397767db5416eef
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2026-02-13 09:37:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,10 +12,12 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 # Lienol
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# openwrt
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 # immortalwrt
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
 # lede
 # sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
@@ -24,43 +26,46 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-defa
 
 #3. Replace with JerryKuKu’s Argon
 # immortalwrt
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 # lede
 # sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
 # Lienol
 # git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+# openwrt
+git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 #4
 # openclash
-rm -rf feeds/luci/applications/luci-app-openclash
+# rm -rf feeds/luci/applications/luci-app-openclash
 
 # homeproxy
-rm -rf feeds/luci/applications/luci-app-homeproxy
+# rm -rf feeds/luci/applications/luci-app-homeproxy
 
 # golang
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # daed
-rm -rf feeds/luci/applications/luci-app-dae
-rm -rf feeds/luci/applications/luci-app-daed
+# rm -rf feeds/luci/applications/luci-app-dae
+# rm -rf feeds/luci/applications/luci-app-daed
 # rm -rf feeds/packages/net/dae
 # rm -rf feeds/packages/net/daed
-# git clone https://github.com/QiuSimons/luci-app-daed package/dae
-# sudo apt-get update
-# sudo apt-get install -y clang llvm npm
-# sudo npm install -g pnpm
-# echo -e "\nCONFIG_DEVEL=y" >> .config
-# echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
-# echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
-# echo -e "\nCONFIG_KERNEL_DEBUG_INFO_BTF=y" >> .config
-# echo -e "\nCONFIG_KERNEL_CGROUPS=y" >> .config
-# echo -e "\nCONFIG_KERNEL_CGROUP_BPF=y" >> .config
-# echo -e "\nCONFIG_KERNEL_BPF_EVENTS=y" >> .config
-# echo -e "\nCONFIG_BPF_TOOLCHAIN_HOST=y" >> .config
-# echo -e "\nCONFIG_KERNEL_XDP_SOCKETS=y" >> .config
-# echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
+sudo apt-get update
+sudo apt-get install -y clang llvm npm
+sudo npm install -g pnpm
+echo -e "\nCONFIG_DEVEL=y" >> .config
+echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
+echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
+echo -e "\nCONFIG_KERNEL_DEBUG_INFO_BTF=y" >> .config
+echo -e "\nCONFIG_KERNEL_CGROUPS=y" >> .config
+echo -e "\nCONFIG_KERNEL_CGROUP_BPF=y" >> .config
+echo -e "\nCONFIG_KERNEL_BPF_EVENTS=y" >> .config
+echo -e "\nCONFIG_BPF_TOOLCHAIN_HOST=y" >> .config
+echo -e "\nCONFIG_KERNEL_XDP_SOCKETS=y" >> .config
+echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
 
 # passwall
 # rm -rf feeds/luci/applications/luci-app-passwall2
