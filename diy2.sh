f0e5a3a9b85b0295643aae40399812bc4a648765
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2026-02-14 23:22:35 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -28,9 +28,37 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # lede
 # sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
 # Official
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
+# golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
+
+# arpbind/autoreboot
+wget -O luci.zip https://github.com/immortalwrt/luci/archive/refs/heads/openwrt-25.12.zip
+unzip -q luci.zip
+mv luci-openwrt-25.12/applications/luci-app-arpbind package/
+mv luci-openwrt-25.12/applications/luci-app-autoreboot package/
+sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-arpbind/Makefile
+sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-autoreboot/Makefile
+rm -rf luci-openwrt-25.12 luci.zip
+
+# openclash
+# rm -rf feeds/luci/applications/luci-app-openclash
+
+# homeproxy
+# rm -rf feeds/luci/applications/luci-app-homeproxy
+
 # daed
+# rm -rf feeds/luci/applications/luci-app-dae
+# rm -rf feeds/luci/applications/luci-app-daed
+# rm -rf feeds/packages/net/dae
+# rm -rf feeds/packages/net/daed
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
+sudo apt-get update
+sudo apt-get install -y clang llvm npm
+sudo npm install -g pnpm
 echo -e "\nCONFIG_DEVEL=y" >> .config
 echo -e "\nCONFIG_KERNEL_DEBUG_INFO=y" >> .config
 echo -e "\nCONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> .config
@@ -41,3 +69,12 @@ echo -e "\nCONFIG_KERNEL_BPF_EVENTS=y" >> .config
 echo -e "\nCONFIG_BPF_TOOLCHAIN_HOST=y" >> .config
 echo -e "\nCONFIG_KERNEL_XDP_SOCKETS=y" >> .config
 echo -e "\nCONFIG_PACKAGE_kmod-xdp-sockets-diag=y" >> .config
+
+# smartdns
+# rm -rf feeds/packages/net/smartdns
+# rm -rf feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+
+# nikki
+# git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
