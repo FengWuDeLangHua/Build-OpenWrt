b79cc6a2833a8a97e70628072f652d96bd0f0ec4
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2026-02-14 14:59:41 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -81,8 +81,8 @@ rm -rf luci-openwrt-25.12 luci.zip
 # rm -rf feeds/luci/applications/luci-app-passwall2
 # rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
 
 # smartdns
 rm -rf feeds/packages/net/smartdns
