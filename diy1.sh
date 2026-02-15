792c00b3c66ae6208092656e079a4ad3f302c1b7
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2026-02-15 13:34:58 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -7,7 +7,7 @@
 #=================================================
 
 # passwall
-sed -i 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
 sed -i 's/Os/O2/g' include/target.mk
