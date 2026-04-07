762335557b14d6980eff39de811b191bf95c2a92
FengWuDeLangHua <83438417+FengWuDeLangHua@users.noreply.github.com>
2026-04-07 20:10:47 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 # Clear the login password
 # immortalwrt
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings
 # lede
 # sed -i 's/"终端"/"TTYD"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
